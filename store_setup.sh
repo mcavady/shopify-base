fd1ecce3bcd70db09670d0845cb7388bcbc61eb1
James McAvady <james@jsmcavady.co.uk>
2023-05-06 03:23:35 +0100

    Update the bash scripting, correcting typo

--- store_setup.sh
+++ store_setup.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
+# Sections to be added to go here
 SHOPIFY_JSM_IMAGE_TILES="node_modules/shopify_jsm-image-tiles/"
+
+#Check for first time run
 echo "First time install ?"
 select yn in "Yes" "No"; do
     case $yn in
-        Yes ) make install; break;;
+        Yes ) break;;
         No ) exit;;
     esac
 done
+
+#needs some work but for now works -
 cp -r "$SHOPIFY_JSM_IMAGE_TILES"scss . \
 && cp -r "$SHOPIFY_JSM_IMAGE_TILES"sections . \
 && cp -r "$SHOPIFY_JSM_IMAGE_TILES"snippets . \
-&& cp -r "$SHOPIFY_JSM_IMAGE_TILES"locals . \
+&& cp -r "$SHOPIFY_JSM_IMAGE_TILES"locales . \
 && cp -r "$SHOPIFY_JSM_IMAGE_TILES"assets .
